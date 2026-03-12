9217626725fa748c07bc5b179e41f5790f09004e
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-12 13:16:41 +0100

    add articles array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-
+const articles = [
+  {
+    id: 1,
+    title: "Introduzione a React",
+    text: "React è una libreria JavaScript utilizzata per costruire interfacce utente.",
+  },
+  {
+    id: 2,
+    title: "Cos'è un componente",
+    text: "I componenti sono blocchi riutilizzabili che compongono un'applicazione React.",
+  },
+  {
+    id: 3,
+    title: "Introduzione a React",
+    text: "Lo state permette ai componenti di React di gestire dati dinamici.",
+  },
+  {
+    id: 4,
+    title: "Introduzione a React",
+    text: "Le props permettono di passare dati da un componente padre a uno figlio.",
+  },
+  {
+    id: 5,
+    title: "Introduzione a React",
+    text: "In React le liste vengono renderizzate utilizzando metodi come map().",
+  },
+];
 
 function App() {
- 
-  return (
-    <>
-
-    </>
-  )
+  return <>
+  </>;
 }
 
-export default App
+export default App;
