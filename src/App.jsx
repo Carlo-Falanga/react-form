41bbcc54f6da60f305bb6f5619ef74f9dee137a0
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-12 14:27:24 +0100

    complete milestone 2: add form logic to insert new articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,31 @@
+import { useState } from "react";
 const articles = [
-  {
-    id: 1,
-    title: "Introduzione a React",
-    text: "React è una libreria JavaScript utilizzata per costruire interfacce utente.",
-  },
-  {
-    id: 2,
-    title: "Cos'è un componente",
-    text: "I componenti sono blocchi riutilizzabili che compongono un'applicazione React.",
-  },
-  {
-    id: 3,
-    title: "Gestire lo stato con useState",
-    text: "Lo state permette ai componenti di React di gestire dati dinamici.",
-  },
-  {
-    id: 4,
-    title: "Le props in React",
-    text: "Le props permettono di passare dati da un componente padre a uno figlio.",
-  },
-  {
-    id: 5,
-    title: "Il rendering delle liste",
-    text: "In React le liste vengono renderizzate utilizzando metodi come map().",
-  },
+  "Introduzione a React",
+  "Cos'è un componente",
+  "Gestire lo stato con useState",
+  "Le props in React",
+  "Il rendering delle liste",
 ];
 
 function App() {
+  const [newArticle, setNewArticle] = useState("");
+  const [article, setArticle] = useState(articles);
+
+  function handlerSubmit(e) {
+    e.preventDefault();
+    setArticle([newArticle, ...article]);
+  }
+
   return (
     <>
-    {/* Articles */}
+      {/* Articles */}
       <div className="container">
         <div className="row mt-5">
           <div className="col">
-            {articles.map((item) => (
-              <div className="card mb-3" key={item.id}>
+            {article.map((item, index) => (
+              <div className="card mb-3" key={index}>
                 <div className="card-body">
-                  <h5 className="card-title">{item.title}</h5>
+                  <h5 className="card-title">{item}</h5>
                   <p className="card-text"></p>
                 </div>
               </div>
@@ -47,10 +36,17 @@ function App() {
 
       {/* Form */}
       <div className="container">
-      <div className="form">
-        <input className="form-control mt-5" type="text" />
-        <div className="btn btn-primary mt-3">Carica</div>
-      </div>
+        <form className="form" onSubmit={handlerSubmit}>
+          <input
+            className="form-control mt-5"
+            type="text"
+            value={newArticle}
+            onChange={(e) => setNewArticle(e.target.value)}
+          />
+          <button className="btn btn-primary mt-3" type="submit">
+            Carica
+          </button>
+        </form>
       </div>
     </>
   );
